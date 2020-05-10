5e39b3b76741cfd24035ad782ed8908abfcce5f5
Usman <akhme.v@gmail.com>
2020-05-10 15:04:34 +0300

    Initial commit

--- app/src/main/java/saloon/app/android/data/models/Article.kt
+++ app/src/main/java/saloon/app/android/data/models/Article.kt
@@ -1,7 +1,6 @@
 package saloon.app.android.data.models
 
 
-data class Article(val id: String, val title: String, val text: Text) {
-}
+data class Article(val id: String, val title: String, val node: Node)
 
-data class Node(val e)
+data class Node(val node: Any)
